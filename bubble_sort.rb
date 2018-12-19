efe49c47455a7bad9b9274b7e00884c65ad8cfcd
dos077 <zeming@dreamofsky.com>
2018-12-19 18:42:35 -0500

    sort_by function also completed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,4 +13,17 @@ def bubble_sort items
     sorting_array
 end
 
-puts bubble_sort([4,3,78,2,0,2])
+def bubble_sort_by items
+    array = items
+    sorted = false
+    until sorted
+        sorted = true
+        for i in 0..array.length-2
+            if yield(array[i], array[i+1]) > 0
+                array[i],array[i+1] = array[i+1], array[i]
+                sorted = false
+            end
+        end
+    end
+    array
+end
